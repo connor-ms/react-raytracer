0d134884388084c001655339d64dc860159d89f9
Connor S <ohvconnor@gmail.com>
2024-08-25 20:36:26 -0700

    Added a friendly message since I can't figure out multithreading

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -27,9 +27,13 @@ const Canvas: React.FC<CanvasProps> = ({ renderer, onRender }) => {
     useEffect(render, []);
 
     return (
-        <div className={`flex flex-col items-center w-[${renderer.cam.imageWidth}px]`}>
+        <div className={`flex flex-col items-center text-center w-[${renderer.cam.imageWidth}px]`}>
             <canvas ref={canvasRef} width={renderer.cam.imageWidth} height={renderer.cam.imageHeight} />
-            <button className="btn btn-primary mt-3" onClick={render}>Render</button>
+            <a className="label-text text-wrap">
+                Note: Rendering a large scene may take some time. Since rendering is done in the main thread, the page will freeze.<br />
+                If it's taking a while, give it some time, grab a coffee, and come back later :)
+            </a>
+            <button className="btn btn-primary my-3" onClick={render}>Render</button>
         </div>
     );
 };
